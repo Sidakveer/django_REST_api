6d1b328e238789bdd188112ebcccdc3f6ec6ae29
Sidakveer <sidakveersingh8@gmail.com>
2022-08-23 11:07:48 -0400

    complete models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,7 +7,29 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
+    def create_user(self, email, name, password=None):
+        """create new user profile"""
+        if not email:
+            raise ValueError("User must have a valid email address")
 
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, name, email, password):
+        """new superuser create and save with its details"""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
--- readme.md
+++ readme.md
@@ -2,6 +2,6 @@
 
 2. setting up our Database using a django  models: update profiles_api/models.py by creating multiple class using object oriented programming
 
-First create a user database model class named userProfile which inherits from two supperclasses and setup the fields,
+First create a user database model class named userProfile which inherits from two parent classes and setup the fields,
 
 second create a user model manager
