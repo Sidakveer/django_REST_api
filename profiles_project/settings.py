d6270c0b7ab2d51d646ce5bc3dbaeb2f758ac05c
Sidakveer <sidakveersingh8@gmail.com>
2022-08-29 14:12:38 -0400

    configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
+STATIC_ROOT = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
